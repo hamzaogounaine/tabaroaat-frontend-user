c9677e2035b06e40cd3d4aa6c5201e2ed3266160
hamzaogounaine <hamz2487@gmail.com>
2025-07-10 16:33:05 +0100

    Change language added

---
+++ src/app/[locale]/page.js
@@ -0,0 +1,27 @@
+"use client"
+
+import AboutSection from "@/components/sections/about-section"
+import ServicesSection from "@/components/sections/services-section"
+import StatisticsSection from "@/components/sections/statistics-section"
+import ProjectsSection from "@/components/sections/projects-section"
+import TestimonialsSection from "@/components/sections/testimonials-section"
+import Footer from "@/components/sections/footer"
+import LoginPage from "@/components/auth/user/Login"
+import HeroSection from "@/components/sections/hero-section"
+
+
+export default function Page() {
+  return (
+    <div>
+      
+      <HeroSection />
+      {/* <LoginPage /> */}
+      <AboutSection />
+      <ServicesSection />
+      <StatisticsSection />
+      <ProjectsSection />
+      <TestimonialsSection />
+      <Footer />
+    </div>
+  );
+}
